ba261e146cb2f5d88d4e14feb27b16351d89b2a3
André Fernandes <andref.pro@gmail.com>
2022-01-26 09:29:26 +0100

    fin dao + id dans person

--- classes/Person.php
+++ classes/Person.php
@@ -4,6 +4,7 @@ class Person {
 
     private string $lastName;
     private string $firstName;
+    private int $id;
 
 
 
@@ -49,6 +50,26 @@ class Person {
     public function setLastName($lastName)
     {
         $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
 
         return $this;
     }
